1b27a8c4480b0b5016bee1381230178774ed3bc8
satheeshpersonal <73348666+satheeshpersonal@users.noreply.github.com>
2025-08-02 19:02:34 +0530

    Add files via upload

---
+++ accounts/admin.py
@@ -0,0 +1,152 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+from .models import CustomUser, Gym, GymMedia, GymTiming, GymEquipment, GymReview, GymFavorite, UserSelectLocation
+
+
+# Inline admin for user select locaiotn
+class UserSelectLocationInline(admin.TabularInline):  # Or use admin.StackedInline for vertical layout
+    model = UserSelectLocation
+    extra = 0  # Number of empty forms to display
+    fields = ('address', 'latitude', 'longitude', 'created_at', 'status')
+    ordering = ('status', '-created_at',)
+    readonly_fields = ('created_at',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # inlines = [UserSelectLocationInline,]  # Add inline here
+    # exclude_fields = ['password', 'last_login']  # fields to exclude
+    list_display = (
+        'username', 
+        'email', 
+        'mobile_number', 
+        'first_name', 
+        'last_name', 
+        'is_staff',
+        'user_type',
+        'status',
+        'created_at'
+    )
+
+
+
+# Inline admin for GymMedia
+class GymMediaInline(admin.TabularInline):  # Or use admin.StackedInline for vertical layout
+    model = GymMedia
+    extra = 0  # Number of empty forms to display
+    fields = ('name', 'description', 'media', 'position', )
+    ordering = ('position',)
+
+# Inline admin for GymTimes
+class GymTimingline(admin.TabularInline):  # Or use admin.StackedInline for vertical layout
+    model = GymTiming
+    extra = 0  # Number of empty forms to display
+    fields = ('day', 'morning_start', 'morning_end', 'evening_start', 'evening_end', 'position', 'status')
+    ordering = ('position',)
+
+# Inline admin for Gym Equipment
+class GymEquipmentline(admin.TabularInline):  # Or use admin.StackedInline for vertical layout
+    model = GymEquipment
+    extra = 0  # Number of empty forms to display
+    fields = ('icon', 'category', 'description', 'position', 'status')
+    ordering = ('position',)
+
+# gym admin
+class GymAdmin(admin.ModelAdmin):
+    # model = Gym
+    # Show these columns in the admin list view
+    list_display = (
+        'name', 
+        'city', 
+        'state', 
+        'country', 
+        'status', 
+        'currency',
+        'owner',
+        'verified_by',
+        'created_at'
+    )
+
+    inlines = [GymMediaInline, GymTimingline, GymEquipmentline]  # Add inline here
+
+    # Enable filters in the sidebar
+    list_filter = ('status', 'city', 'state', 'currency', 'verified_by')
+
+    # Make fields searchable
+    search_fields = ('name', 'city', 'state', 'country', 'owner__email')
+
+    # Automatically set read-only fields
+    readonly_fields = ('created_at', 'updated_at', 'gym_id')
+
+    # Optional: organize fields into sections
+    fieldsets = (
+        ("Basic Info", {
+            'fields': ('name', 'description', 'profile_icon')
+        }),
+        ("Location", {
+            'fields': ('address', 'city', 'state', 'country', 'zip_code', 'latitude', 'longitude')
+        }),
+        ("Pricing & Status", {
+            'fields': ('per_session_cost', 'currency', 'premium_type', 'status')
+        }),
+        ("Feature", {
+            'fields': ('feature',)
+        }),
+        ("Ownership", {
+            'fields': ('owner', 'verified_by')
+        }),
+        ("System Info", {
+            'fields': ('gym_id', 'created_at', 'updated_at')
+        }),
+    )
+
+
+# gym Review
+class GymReviewAdmin(admin.ModelAdmin):
+    # model = Gym
+    # Show these columns in the admin list view
+    # list_display_links = None
+    list_display = (
+        'id',
+        'user',
+        'gym',
+        'rating', 
+        'created_at',
+        'status'
+    )
+
+    # Make fields searchable
+    search_fields = ('user', 'gym', 'rating')
+    list_filter = ('user', 'gym', 'rating', 'status')
+
+    list_editable = ('status',)
+    ordering = ('-created_at',)
+
+
+# gym Review
+class GymFavoriteAdmin(admin.ModelAdmin):
+    list_display_links = None
+    list_display = (
+        'user',
+        'gym',
+        'created_at',
+    )
+
+    # Make fields searchable
+    search_fields = ('user', 'gym')
+    list_filter = ('user', 'gym')
+
+    # list_editable = ('status',)
+    ordering = ('-created_at',)
+
+
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Gym, GymAdmin)
+admin.site.register(GymReview, GymReviewAdmin)
+admin.site.register(GymFavorite, GymFavoriteAdmin)
+
+
+
---
+++ accounts/serializers.py
@@ -0,0 +1,150 @@
+from rest_framework import serializers
+from .models import CustomUser, UserSelectLocation, GymMedia, GymEquipment, Gym, GymTiming, GymReview
+from lookups.models import GymFeature
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'username', 'email', 'mobile_number', 'ip_country', 'ip_state', 'ip_city', 'status', 'user_type', 'login_type', 'profile_completed', 'profile_icon', ]
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'username', 'email', 'mobile_number', 'profile_icon', 'new_to_gym', 'height', 'weight', 'dob', 'gender', 'address', 'country', 'state', 'city', 'profile_completed']
+
+
+class UserSelectLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSelectLocation
+        fields = '__all__'
+        # fields = ['user', 'address', 'latitude', 'longitude', 'status', 'created_at']
+        read_only_fields = ['created_at']
+
+
+class GymMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GymMedia
+        fields = ['name', 'media', 'description']
+
+class GymEquipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GymEquipment
+        fields = ['category', 'description', 'icon']
+
+class GymFeatureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GymFeature
+        fields = ['name', 'icon', 'details']
+
+class GymTimingSerializer(serializers.ModelSerializer):
+    day = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = GymTiming
+        fields = ['day', 'morning_start', 'morning_end', 'evening_start', 'evening_end']
+    
+    def get_day(self, obj):
+        return obj.get_day_display()
+
+class GymOwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'first_name', 'last_name', 'mobile_number']
+
+
+class GymDetailsSerializer(serializers.ModelSerializer):
+    media = serializers.SerializerMethodField()
+    feature = serializers.SerializerMethodField()
+    equipment = serializers.SerializerMethodField()
+    gymtiming = serializers.SerializerMethodField()
+    gymowner = serializers.SerializerMethodField()
+    # rating = serializers.SerializerMethodField()
+    # distance_km = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Gym
+        fields = ['id', 'gym_id', 'name', 'description', 'address', 'city', 'state', 'latitude', 'longitude', 'premium_type', 'media', 'feature', 'equipment', 'gymtiming', 'gymowner' #distance_km, 'rating'
+        ]
+
+    def get_media(self, obj):
+        return GymMediaSerializer(obj.gymmedia_set.all().order_by("position"), many=True).data
+    
+    def get_feature(self, obj):
+        return GymFeatureSerializer(obj.feature.filter(status='A').order_by("position")[:6], many=True).data
+    
+    def get_equipment(self, obj):
+        return GymEquipmentSerializer(obj.gymequipment_set.filter(status='A').order_by("position")[:6], many=True).data
+    
+    def get_gymtiming(self, obj):
+        return GymTimingSerializer(obj.gymtiming_set.filter(status='A').order_by("position"), many=True).data
+    
+    def get_gymowner(self, obj):
+        return GymOwnerSerializer(obj.owner).data
+
+    # def get_rating(self, obj):
+    #     reviews = obj.gymreview_set.all()
+    #     if reviews:
+    #         return round(sum(r.rating for r in reviews) / len(reviews), 1)
+    #     return 0
+
+    # def get_distance_km(self, obj):
+    #     user_lat = self.context.get('user_lat')
+    #     user_lon = self.context.get('user_lon')
+    #     if user_lat and user_lon and obj.latitude and obj.longitude:
+    #         from geopy.distance import geodesic
+    #         return round(geodesic((user_lat, user_lon), (obj.latitude, obj.longitude)).km, 2)
+    #     return None
+
+
+class GymListSerializer(serializers.ModelSerializer):
+    media = serializers.SerializerMethodField()
+    feature = serializers.SerializerMethodField()
+    gymowner = serializers.SerializerMethodField()
+    distance = serializers.SerializerMethodField()
+    rating = serializers.SerializerMethodField()
+    # distance_km = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Gym
+        fields = ['id', 'gym_id', 'name', 'description', 'address', 'city', 'state', 'premium_type', 'media', 'feature', 'gymowner', 'distance', 'rating']
+
+    def get_media(self, obj):
+        return GymMediaSerializer(obj.gymmedia_set.all().order_by("position"), many=True).data
+    
+    def get_feature(self, obj):
+        return GymFeatureSerializer(obj.feature.filter(status='A').order_by("position"), many=True).data
+    
+    def get_gymowner(self, obj):
+        return GymOwnerSerializer(obj.owner).data
+
+    def get_distance(self, obj):
+        return getattr(obj, 'distance', None)
+    
+    def get_rating(self, obj):
+        reviews = obj.gymreview_set.all()
+        if reviews:
+            total = len(reviews)
+            average = round(sum(r.rating for r in reviews) / total, 1)
+            return {"average":average, "total":total}
+
+        return {"average":0, "total":0}
+    
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'first_name', 'last_name', 'mobile_number', 'profile_icon']
+
+
+class GymReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GymReview
+        fields = '__all__'
+        read_only_fields = ['created_at']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['user'] = UserDetailsSerializer(instance.user).data
+
+
+        return data

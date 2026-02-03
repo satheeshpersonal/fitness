45cf98045cd58dea00191d86bdb960ba8a0505ce
satheeshpersonal <73348666+satheeshpersonal@users.noreply.github.com>
2026-02-03 11:57:20 +0530

    Add files via upload

--- lookups/functions.py
+++ lookups/functions.py
@@ -2,27 +2,61 @@ from django.core.mail import EmailMessage, BadHeaderError
 import smtplib
 from decouple import config
 import requests
+from threading import Thread
 
-def send_email(subject, body, to_email, cc_email=None):
-    email = EmailMessage(
-        subject=subject,
-        body=body,
-        from_email=config('DEFAULT_FROM_EMAIL'),
-        to=to_email,
-    )
-    # email.send()
+# def send_email_smtp(subject, body, to_email, cc_email=None):
+#     email = EmailMessage(
+#         subject=subject,
+#         body=body,
+#         from_email=config('DEFAULT_FROM_EMAIL'),
+#         to=to_email,
+#     )
+#     # email.send()
+#     try:
+#         result = email.send(fail_silently=False)
+#         if result == 1:
+#             print("Email sent successfully ")
+#         else:
+#             print("Email not sent ")
+#     except BadHeaderError as e:
+#         print(f"Invalid header found: : {e}")
+#     except smtplib.SMTPException as e:
+#         print(f"SMTP error: {e}")
+#     except Exception as e:
+#         print(f"Other error: {e}")
+
+def send_email_api(subject, body, to_email, cc_email=None):
     try:
-        result = email.send(fail_silently=False)
-        if result == 1:
-            print("Email sent successfully ✅")
+        if isinstance(to_email, list): # if to email is list 
+            to_list = [{"email": email} for email in to_email]
         else:
-            print("Email not sent ❌")
-    except BadHeaderError as e:
-        print(f"Invalid header found: : {e}")
-    except smtplib.SMTPException as e:
-        print(f"SMTP error: {e}")
+            to_list = [{"email": to_email}]
+            
+        response = requests.post(
+            "https://api.brevo.com/v3/smtp/email",
+            headers={
+                "api-key": config("EMAIL_HOST_API_KEY"),
+                "Content-Type": "application/json",
+            },
+            json={
+                "sender": {
+                    "email": config("DEFAULT_FROM_EMAIL"),
+                    "name": "Fitness App"
+                },
+                "to": to_list,
+                "subject": subject,
+                "htmlContent": f"<p>{body}</p>"
+            },
+            timeout=10  # prevents long hanging
+        )
+
+        if response.status_code == 201:
+            print("Email sent via API ")
+        else:
+            print("Brevo error ", response.text)
+
     except Exception as e:
-        print(f"Other error: {e}")
+        print("Email API failed ", e)
 
 
 def send_sms(otp, to_number):
@@ -46,4 +80,10 @@ def send_sms(otp, to_number):
 
 
 def send_template_email(subject, body, to_email, cc_email=None, template_key = None):
-    send_email(subject, body, to_email, cc_email)
+    # send_email_smtp(subject, body, to_email, cc_email)
+    # print(to_email)
+    Thread(
+        target=send_email_api,
+        args=(subject, body, to_email),
+        daemon=True
+    ).start()

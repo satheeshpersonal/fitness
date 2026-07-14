fc30ecf2c4a109c4a1167b1531c891a705f1ba87
satheeshpersonal <73348666+satheeshpersonal@users.noreply.github.com>
2026-07-14 15:46:53 +0530

    Update settings.py

--- FitnessApp/settings.py
+++ FitnessApp/settings.py
@@ -167,6 +167,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    "version": 1,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
+
 # --------------------------------------------------
 # Internationalization
 # --------------------------------------------------
